078ed56b649efb3211ee10dbf8375075289d327f
Ruslan <sabackas@yandex.ru>
2023-06-14 17:48:40 +0400

    add Task38.py

--- Task38.py
+++ Task38.py
@@ -1,92 +1,150 @@
-# Задача 38: 
-# Дополнить телефонный справочник возможностью изменения и удаления данных. 
-# Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал для изменения и удаления данных
+# Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных.
+# Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал
+# для изменения и удаления данных.
 
-# Показывает информацию в файле
-def show_data(filename):
-    print("\nПП | ФИО | Телефон")
-    with open(filename, "r", encoding="utf-8") as data:
-        print(data.read())
-    print("")
 
-# Записывает информацию в файл
-def export_data(filename):
-    with open(filename, "r", encoding="utf-8") as data:
-        tel_file = data.read()
-    num = len(tel_file.split("\n"))
-    with open(filename, "a", encoding="utf-8") as data: 
-        fio = input("Введите ФИО: ")
-        phone_number = input("Введите номер телефона: ")
-        data.write(f"{num} | {fio} | {phone_number}\n")
-        print(f"Добавлена запись : {num} | {fio} | {phone_number}\n")
 
-# Изменяет информацию из файла
-def edit_data(filename):
-    print("\nПП | ФИО | Телефон")
-    with open(filename, "r", encoding='utf-8') as data:
-        tel_book = data.read()
-    print(tel_book)
-    print("")
-    index_delete_data = int(input("Введите номер строки для редактирования: ")) - 1
-    tel_book_lines = tel_book.split("\n")
-    edit_tel_book_lines = tel_book_lines[index_delete_data]
-    elements = edit_tel_book_lines.split(" | ")
-    fio = input("Введите ФИО: ")
-    phone = input("Введите номер телефона: ")
-    num = elements[0]
-    if len(fio) == 0:
-        fio = elements[1]
-    if len(phone) == 0:
-        phone = elements[2]
-    edited_line = f"{num} | {fio} | {phone}"
-    tel_book_lines[index_delete_data] = edited_line
-    print(f"Запись - {edit_tel_book_lines}, изменена на - {edited_line}\n")
-    with open(filename, "w", encoding='utf-8') as f:
-        f.write("\n".join(tel_book_lines))
+def print_records(file_name: str):
+    with open(file_name, 'r', encoding='utf-8') as data:
+        for line in data:
+            print(*line.split(';'), end='')
 
-# Удаляет информацию из файла
-def delete_data(filename):
-    print("\nПП | ФИО | Телефон")
-    with open(filename, "r", encoding="utf-8") as data:
-        tel_book = data.read()
-        print(tel_book)
-    print("")
-    index_delete_data = int(input("Введите номер строки для удаления: ")) - 1
-    tel_book_lines = tel_book.split("\n")
-    del_tel_book_lines = tel_book_lines[index_delete_data]
-    tel_book_lines.pop(index_delete_data)
-    print(f"Удалена запись: {del_tel_book_lines}\n")
-    with open(filename, "w", encoding='utf-8') as data:
-        data.write("\n".join(tel_book_lines))
 
-def main():
-    my_choice = -1
-    file_tel = "tel.txt"
+def input_records(file_name: str):
+    with open(file_name, 'r+', encoding='utf-8') as data:
+        record_id = 0
+        for line in data:
+            if line != '':
+                record_id = line.split(';', 1)[0]
+        print('Введите фамилию, имя, отчество, номер телефона через пробел')
+        line = f'{int(record_id) + 1};' + ';'.join(input().split()[:4]) + ';\n'
+        confirm = confirmation('добавление записи')
+        if confirm == 'y':
+            data.write(line)
 
-    # Создает файл если его нет в папке
-    with open(file_tel, "a", encoding="utf-8") as file:
-         file.write("")
 
-    while my_choice != 0:
-        print("Выберите одно из действий:")
-        print("1 - Вывести инфо на экран")
-        print("2 - Произвести экпорт данных")
-        print("3 - Произвести изменение данных")
-        print("4 - Произвести удаление данных")
-        print("0 - Выход из программы")
-        action = int(input("Действие: "))
-        if action == 1:
-            show_data(file_tel)
-        elif action == 2:
-            export_data(file_tel)
-        elif action == 3:
-            edit_data(file_tel)
-        elif action == 4:
-            delete_data(file_tel)
+def find_char():
+    print('Выберите характеристику:')
+    print('0 - id, 1 - фамилия, 2 - имя, 3 - отчество, 4 - номер, q - выйти')
+    char = input()
+    while char not in ('0', '1', '2', '3', '4', 'q'):
+        print('Введены неверные данные')
+        print('Выберите характеристику:')
+        print('0 - id, 1 - фамилия, 2 - имя, 3 - отчество, 4 - номер, q - выйти')
+        char = input()
+    if char != 'q':
+        inp = input('Введите значение\n')
+        return char, inp
+    else:
+        return 'q', 'q'
+
+
+def find_records(file_name: str, char, condition):
+    if condition != 'q':
+        printed = False
+        with open(file_name, 'r', encoding='utf-8') as data:
+            for line in data:
+                if condition == line.split(';')[int(char)]:
+                    print(*line.split(';'))
+                    printed = True
+        if not printed:
+            print("Не найдено")
+        return printed
+
+
+def check_id_record(file_name: str, text: str):
+    decision = input(
+        f'Вы знаете id записи которую хотите {text}? 1 - да, 2 - нет, q - выйти\n')
+    while decision not in ('1', 'q'):
+        if decision != '2':
+            print('Введены неверные данные')
         else:
-            my_choice = 0
+            find_records(path, *find_char())
+        decision = input(
+            f'Вы знаете id записи которую хотите {text}? 1 - да, 2 - нет, q - выйти\n')
+    if decision == '1':
+        record_id = input('Введите id, q - выйти\n')
+        while not find_records(file_name, '0', record_id) and record_id != 'q':
+            record_id = input('Введите id, q - выйти\n')
+        return record_id
+    return decision
 
-    print("До свидания")
 
-if __name__ == "__main__":
-    main()
+def confirmation(text: str):
+    confirm = input(
+        f"Подтвердите {text} записи: y - да, n - нет\n")
+    while confirm not in ('y', 'n'):
+        print('Введены неверные данные')
+        confirm = input(
+            f"Подтвердите {text} записи: y - да, n - нет\n")
+    return confirm
+
+
+def replace_record_line(file_name: str, record_id, replaced_line: str):
+    replaced = ''
+    with open(file_name, 'r', encoding='utf-8') as data:
+        for line in data:
+            replaced += line
+            if record_id == line.split(';', 1)[0]:
+                replaced = replaced.replace(line, replaced_line)
+    with open(file_name, 'w', encoding='utf-8') as data:
+        data.write(replaced)
+
+
+def change_records(file_name: str):
+    record_id = check_id_record(file_name, 'изменить')
+    if record_id != 'q':
+        replaced_line = f'{int(record_id)};' + ';'.join(
+            input('Введите фамилию, имя, отчество, номер телефона через пробел\n').split()[:4]) + ';\n'
+        confirm = confirmation('изменение')
+        if confirm == 'y':
+            replace_record_line(file_name, record_id, replaced_line)
+
+
+def delete_records(file_name: str):
+    record_id = check_id_record(file_name, 'удалить')
+    if record_id != 'q':
+        confirm = confirmation('удаление')
+        if confirm == 'y':
+            replace_record_line(file_name, record_id, '')
+
+
+path = 'phone_book.txt'
+
+try:                        # исключения try/except/finally
+    file = open(path, 'r')  # открыть файл
+except IOError:             # если нет файла он создается
+    print('Создан новый справочник -> phone_book.txt ')
+    file = open(path, 'w')
+finally:
+    file.close()
+
+actions = {'1': 'список',
+           '2': 'запись',
+           '3': 'поиск',
+           '4': 'изменение',
+           '5': 'удаление',
+           'q': 'выход'}
+
+action = None
+while action != 'q':
+    print('Какое действие хотите совершить?',
+          *[f'{i} - {actions[i]}' for i in actions])
+    action = input()
+    while action not in actions:
+        print('Какое действие хотите совершить?',
+              *[f'{i} - {actions[i]}' for i in actions])
+        action = input()
+        if action not in actions:
+            print('Введены неверные данные')
+    if action != 'q':
+        if action == '1':
+            print_records(path)
+        elif action == '2':
+            input_records(path)
+        elif action == '3':
+            find_records(path, *find_char())
+        elif action == '4':
+            change_records(path)
+        elif action == '5':
+            delete_records(path)
